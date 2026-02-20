6e5cbca6e93bf0d5f85d6919154395ef5730c0a7
Abel Santillan Rodriguez <abelsantillanrdz@gmail.com>
2026-02-20 12:47:57 -0600

    Enhance project structure: update .gitignore, add pyproject.toml and requirements.txt, and refine setup.py for better package management

--- Scraping_Bancos_MX/__init__.py
+++ Scraping_Bancos_MX/__init__.py
@@ -1,3 +1,11 @@
+"""
+Scraping_Bancos_MX - Librería para extraer datos de estados de cuenta bancarios mexicanos.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Abel Santillan Rodriguez"
+__email__ = "abelsantillanrdz@gmail.com"
+
 from .Funciones_Afirme import *
 from .Funciones_BBVA import *
 from .Funciones_Banorte import *
@@ -14,3 +22,22 @@ from .Funciones_MercadoPago import *
 from .Funciones_Nu import *
 from .Funciones_Bancoppel import *
 from .Funciones_Banjercito import *
+
+__all__ = [
+    "Scrap_Estado_Afirme",
+    "Scrap_Estado_BBVA",
+    "Scrap_Estado_Banorte",
+    "Scrap_Estado_BanRegio",
+    "Scrap_Estado_BanBajio",
+    "Scrap_Estado_Inbursa",
+    "Scrap_Estado_Santander",
+    "Scrap_Estado_Scotiabank",
+    "Scrap_Estado_HeyBanco",
+    "Scrap_Estado_Banamex",
+    "Scrap_Estado_Azteca",
+    "Scrap_Estado_HSBC",
+    "Scrap_Estado_MercadoPago",
+    "Scrap_Estado_Nu",
+    "Scrap_Estado_Bancoppel",
+    "Scrap_Estado_Banjercito",
+]
--- setup.py
+++ setup.py
@@ -4,24 +4,37 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0' 
-PACKAGE_NAME = 'Scraping_Bancos_MX' 
-AUTHOR = 'Abel Santillan Rodriguez' 
-AUTHOR_EMAIL = 'abelsantillanrdz@gmail.com' 
-URL = 'https://github.com/abelsr-kosmos/Scraping_Bancos_Mx' 
+VERSION = '0.1.0'
+PACKAGE_NAME = 'Scraping-Bancos-MX'
+AUTHOR = 'Abel Santillan Rodriguez'
+AUTHOR_EMAIL = 'abelsantillanrdz@gmail.com'
+URL = 'https://github.com/abelsr-kosmos/Scraping_Bancos_Mx'
 
-LICENSE = 'MIT' #Tipo de licencia
-DESCRIPTION = 'Librería para extraer datos de movimientos de los estados de cuenta de bancos Mexicanos' #Descripción corta
-LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LICENSE = 'MIT'
+DESCRIPTION = 'Librería para extraer datos de movimientos de los estados de cuenta de bancos Mexicanos'
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
 LONG_DESC_TYPE = "text/markdown"
 
-
-#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
 INSTALL_REQUIRES = [
-        'pymupdf',
-        "pdfplumber",
-        "pandas"
-      ]
+    'pdfplumber>=0.10.0',
+    'pandas>=1.5.0',
+    'numpy>=1.21.0'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Office/Business :: Financial',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+KEYWORDS = 'pdf scraping bank statement extractor mexico bancos'
 
 setup(
     name=PACKAGE_NAME,
@@ -32,8 +45,12 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     url=URL,
-    install_requires=INSTALL_REQUIRES,
     license=LICENSE,
-    packages=find_packages(),
-    include_package_data=True
+    packages=find_packages(exclude=['tests', 'notebooks', 'benchmarks']),
+    python_requires='>=3.9',
+    install_requires=INSTALL_REQUIRES,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    include_package_data=True,
+    zip_safe=False,
 )
